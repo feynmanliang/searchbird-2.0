b744f98df5c9d3bc24708647cfaddc94a059e43b
Feynman Liang <fliang@databricks.com>
2015-08-14 18:08:48 -0700

    Add twitter server

--- build.sbt
+++ build.sbt
@@ -11,6 +11,7 @@ libraryDependencies += "org.apache.thrift" % "libthrift" % "0.9.2"
 libraryDependencies += "com.twitter" %% "finagle-thrift" % "6.27.0"
 libraryDependencies += "com.twitter" %% "scrooge-core" % "3.3.2"
 libraryDependencies += "com.twitter" %% "twitter-server" % "1.12.0"
+libraryDependencies += "com.twitter" %% "finagle-stats" % "6.27.0"
 
 
 
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,13 +2,16 @@ package com.feynmanliang.searchbird
 
 import com.feynmanliang.searchbird.config.SearchbirdServiceConfig
 import com.twitter.finagle.Thrift
+import com.twitter.server.TwitterServer
 import com.twitter.util.Await
 
-object Main {
-  def main(args: Array[String]): Unit = {
+object Main extends TwitterServer {
+  def main(): Unit = {
     val config = new SearchbirdServiceConfig()
     val service = new SearchbirdServiceImpl(config)
     val server = Thrift.serveIface("localhost:" + config.thriftPort, service)
+
     Await.ready(server)
+    Await.ready(adminHttpServer)
   }
 }
